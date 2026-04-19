2334f0e90b97943f06a3845874c26eabd0d9a72c
mintychochip <jlo2@csub.edu>
2026-04-19 10:06:23 -0700

    fix: Dockerfile monorepo build + nginx location /

--- apps/web/src/components/Navbar.tsx
+++ apps/web/src/components/Navbar.tsx
@@ -66,6 +66,7 @@ export default function Navbar() {
               >
                 My Orders
               </Link>
+              <LogoutButton />
             </>
           ) : (
             <>
