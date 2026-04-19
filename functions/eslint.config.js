6b02b281a17b3746f5ae55f544a1d37db68ecaf5
mintychochip <jlo2@csub.edu>
2026-04-19 01:18:36 -0700

    build: update functions eslint config and dependencies

--- functions/eslint.config.js
+++ functions/eslint.config.js
@@ -1,27 +1,22 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import typescriptParser from "@typescript-eslint/parser";
+import tseslint from "typescript-eslint";
 
-export default [
+export default tseslint.config(
   {
     ignores: ["node_modules/**", "lib/**", "dist/**", "*.config.js"],
   },
+  tseslint.configs.recommended,
   {
     files: ["**/*.ts"],
     languageOptions: {
-      parser: typescriptParser,
       parserOptions: {
         ecmaVersion: 2022,
         sourceType: "module",
       },
     },
-    plugins: {
-      "@typescript-eslint": typescriptEslint,
-    },
     rules: {
       "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
       "@typescript-eslint/no-explicit-any": "warn",
       "no-console": "off",
-      "@typescript-eslint/no-explicit-any": "warn",
     },
-  },
-];
+  }
+);
